b0298a5be0f13147696343fc37df9fb77413feb0
oliviacoca <60169169+oliviacoca@users.noreply.github.com>
2021-04-28 19:42:07 -0600

    Create TreeObject.java

---
+++ TreeObject.java
@@ -0,0 +1,18 @@
+public class TreeObject{
+
+private long data; 
+private int sequenceData; 
+private int duplicates; 
+
+  public TreeObject( long data, int sequenceData){
+    this.data = data; 
+    this.sequenceData = sequenceData; 
+    this.duplicates = 1; 
+    
+    
+  
+  }
+
+
+
+}
