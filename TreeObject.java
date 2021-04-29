a400475645ca1342f27adb02e7018ee30c0a10fc
oliviacoca <60169169+oliviacoca@users.noreply.github.com>
2021-04-28 19:58:33 -0600

    Update TreeObject.java

--- TreeObject.java
+++ TreeObject.java
@@ -1,16 +1,36 @@
 public class TreeObject{
 
-private long data; 
+private long key; 
 private int sequenceData; 
 private int duplicates; 
 
-  public TreeObject( long data, int sequenceData){
-    this.data = data; 
+  public TreeObject(long key, int sequenceData){
+    this.key = key; 
     this.sequenceData = sequenceData; 
-    this.duplicates = 1; 
-    
-    
+    this.duplicates = 1;
+  }
   
+  //NEED TO DO
+  public int increaseDuplicates(){
+  }
+  
+  public void setDuplicates(){
+  }
+  
+  public long  getKey(){
+  }
+  
+  public void setKey(long key){
+    
+  }
+  
+  public int compareTo(TreeObject t){
+    if(this.key > t.getKey()){
+      return 1; //
+    }else if(this.key < t.getKey()){
+      return -1; 
+    }else{
+      return 0; 
   }
 
 
