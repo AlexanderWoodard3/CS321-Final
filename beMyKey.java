8d76159463cefdb0c68201a2a97c06937297b8f5
oliviacoca <60169169+oliviacoca@users.noreply.github.com>
2021-05-05 20:48:31 -0600

    Add files via upload

---
+++ beMyKey.java
@@ -0,0 +1,19 @@
+
+public class beMyKey {
+	public Long keyValue;
+	public Long frequency; 
+	
+	
+	public beMyKey(Long aKey) {
+		keyValue = aKey;
+		frequency = 1L; 
+	}
+	
+	public String sequence() {
+		// TODO Auto-generated method stub
+		//converts to ACTG
+		
+		return null;
+	}
+	
+}
